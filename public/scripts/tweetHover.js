879e1d6e81c5349c726c5b0cfaf3781fd8759a16
Bryan Kenny <b.93.kenny@gmail.com>
2018-11-07 01:59:03 +0000

    got code review and added the icons

--- public/scripts/tweetHover.js
+++ public/scripts/tweetHover.js
@@ -2,12 +2,14 @@ $(document).ready (function() {
   $(".tweet-list").on("mouseover", ".tweet-container", function(event) {
   $(this).addClass("highlight");
     console.log(this);
+  $(this).find(".iconset").show();
     event.stopPropagation();
    });
 
     $(".tweet-list").on("mouseleave", ".tweet-container", function(event) {
      console.log("bye mouse");
     $(this).removeClass("highlight");
+    $(this).find(".iconset").hide();
       event.stopPropagation();
   })
 })
@@ -16,4 +18,5 @@ $(document).ready (function() {
 // the $(document).ready fires when the DOM is ready, preventing things from happening before everything is rendered on the page
 // .addClass adds the specified class to each element in the set of matched elemnts, in this case this, which is referencing article with the class "tweet-container highlight"
 // (tweet-list).on("mouseLeave") and the .removeClass afterwards are whats responsible for the highlight going away on mouseleave
+// $(this).find(".iconset") show/hide makes it so the icons appear when hightligh is activated and dissapear when the mouse is off
 // event.stopPropagation prevents the event from "bubbling" up the DOM tree preventing any parent handlers from being notified of the event
