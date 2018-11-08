b64f0ae6a741ad199fe89c39d622004907052783
Bryan Kenny <b.93.kenny@gmail.com>
2018-11-08 22:02:17 +0000

    got error messages displaying properly, and the formcleared upon submission. need to fix so error colors work consistentely

---
+++ public/scripts/newTweetSubmission.js
@@ -0,0 +1,59 @@
+ console.log("I am submit-tweet.js");
+ console.log(this);
+
+  $(document).ready( function() {
+  $('#nav-bar').on('click', '.compose', function() {
+  $('.new-tweet').slideToggle();
+  $('.new-tweet').find('textarea').select();
+});
+
+  $('.new-tweet').on('submit', 'form', function(event) {
+    event.preventDefault();
+    let userTweet = $(this).find('textarea').val();
+
+
+      if (!userTweet) {
+         $(this).find('.userTweetForm').text("Empty Tweet!").addClass("form-error");
+         return;
+
+      } else if (userTweet.length > 140) {
+         $(this).find('.userTweetForm').text("Too Many Characters").addClass("form-error");
+         return;
+      }
+  $(this).find('textarea').val("");
+  $(this).find('textarea').trigger('input');
+
+      console.log("tweet sent");
+  $(this).find('.userTweetForm').text("Tweet sent!").addClass("form-ok");
+
+//in the event of too many characters or an empty text field we will change the html elements class so we can appropriately style it to make the error clear
+
+    $.ajax({
+      url: '/tweets/',
+      data: $(this).serialize(),
+      method: 'POST',
+      success: function () {
+         console.log('tweet submit OK');
+    $.ajax( {
+      url: '/tweets',
+      method: 'GET',
+      success: function(obj) {
+      renderTweets(obj);
+          }
+        });
+      }
+    });
+  });
+ });
+
+ // look into complete/ turning ajax into a function
+ //in this context this is referring to to <form action = "/tweets" method=POST></form>
+ // line 4 is saying upon submission of the form that submits a new tweet,
+ // event.preventDefault prevents the default action of whichever event the method is being called on
+ // .find = Get the descendants of each element in the current set of matched elements, filtered by a selector, jQuery object, or element.
+ // .val = Get the current value of the first element in the set of matched elements or set the value of every matched element. in this context, current value of the current object in /tweets, represented by this
+ // the .ajax logic is uded to perform an asynchronous http request, so we can do things like have new elements load on a page without it reloading
+ // The .serialize() method creates a text string in standard URL-encoded notation. It can act on a jQuery object that has selected individual form controls. its purpose here is to save the state of an object so it can be recreated when needed.
+ // In this care we are serializing the json data that is being referenced iby this, which is the array of tweet objects found at /tweets
+  // we gave the hide compose tweet button a class of compose so we could reference it within the anonymous function that we call .slideToggle on
+  //
