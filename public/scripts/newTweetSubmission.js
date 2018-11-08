4b3984c4d595bb9837ebc0fdd55ce674c2cbdf92
Bryan Kenny <b.93.kenny@gmail.com>
2018-11-08 23:10:07 +0000

    submit is broken. have to fix

--- public/scripts/newTweetSubmission.js
+++ public/scripts/newTweetSubmission.js
@@ -10,21 +10,24 @@
   $('.new-tweet').on('submit', 'form', function(event) {
     event.preventDefault();
     let userTweet = $(this).find('textarea').val();
+     $(this).find('textarea').val("");
+    $(this).find('textarea').trigger('input');
 
+// above we are defining userTweet as the value of what is being submitted into the new-tweet text-area box
+// above that we are calling the slideToggle function on the form with the .compose class when it is clicked
 
-      if (!userTweet) {
-         $(this).find('.userTweetForm').text("Empty Tweet!").addClass("form-error");
+        if (!userTweet) {
+         $(this).find('.userTweetForm').text("Empty Tweet!").addClass("error-form");
          return;
 
       } else if (userTweet.length > 140) {
-         $(this).find('.userTweetForm').text("Too Many Characters").addClass("form-error");
+         $(this).find('.userTweetForm').text("Too Many Characters").addClass("error-form");
          return;
       }
+
   $(this).find('textarea').val("");
   $(this).find('textarea').trigger('input');
-
-      console.log("tweet sent");
-  $(this).find('.userTweetForm').text("Tweet sent!").addClass("form-ok");
+  $(this).find('.userTweetForm').text("Tweet'd!").addClass("working-form");
 
 //in the event of too many characters or an empty text field we will change the html elements class so we can appropriately style it to make the error clear
 
@@ -48,12 +51,10 @@
 
  // look into complete/ turning ajax into a function
  //in this context this is referring to to <form action = "/tweets" method=POST></form>
- // line 4 is saying upon submission of the form that submits a new tweet,
  // event.preventDefault prevents the default action of whichever event the method is being called on
  // .find = Get the descendants of each element in the current set of matched elements, filtered by a selector, jQuery object, or element.
  // .val = Get the current value of the first element in the set of matched elements or set the value of every matched element. in this context, current value of the current object in /tweets, represented by this
  // the .ajax logic is uded to perform an asynchronous http request, so we can do things like have new elements load on a page without it reloading
  // The .serialize() method creates a text string in standard URL-encoded notation. It can act on a jQuery object that has selected individual form controls. its purpose here is to save the state of an object so it can be recreated when needed.
- // In this care we are serializing the json data that is being referenced iby this, which is the array of tweet objects found at /tweets
-  // we gave the hide compose tweet button a class of compose so we could reference it within the anonymous function that we call .slideToggle on
-  //
+ // In this case we are serializing the json data that is being referenced iby this, which is the array of tweet objects found at /tweets
+ //
