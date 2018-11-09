6eeb21c17b51a18cdcd18020b800ce57fa0102be
Bryan Kenny <b.93.kenny@gmail.com>
2018-11-09 23:38:12 +0000

    trying to merge brnaches

--- mongo_example.js
+++ mongo_example.js
@@ -9,16 +9,33 @@ MongoClient.connect(MONGODB_URI, (err, db) => {
     throw err;
   }
 
-  // ==> We have a connection to the "test-tweets" db,
-   console.log(`Connected to mongodb: ${MONGODB_URI}`);
+  // We have a connection to the "tweeter" db, starting here.
+  console.log(`Connected to mongodb: ${MONGODB_URI}`);
 
-  // ==> Let's "get all the tweets". In Mongo-speak, we "find" them.
-    db.collection("tweets").find().toArray((err, results) => {
+  // ==> Refactored and wrapped as new, tweet-specific function:
+
+  function getTweets(callback) {
+    db.collection("tweets").find().toArray((err, tweets) => {
+      if (err) {
+        return callback(err);
+      }
+      callback(null, tweets);
+    });
+  }
+
+  // ==> Later it can be invoked. Remember even if you pass
+  //     `getTweets` to another scope, it still has closure over
+  //     `db`, so it will still work. Yay!
+
+  getTweets((err, tweets) => {
     if (err) throw err;
 
-    console.log("results array: ", results);
+    console.log("Logging each tweet:");
+    for (let tweet of tweets) {
+      console.log(tweet);
+    }
 
-    // This is the end...
     db.close();
   });
-  });
+
+});
